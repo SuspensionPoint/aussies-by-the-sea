9b4b9417113627a5499e4d41e2ef10211cddeaa6
Jeremy Bailey <jbailey@oxefit.com>
2020-12-01 16:34:53 -0600

    less confetti

--- src/App.vue
+++ src/App.vue
@@ -414,7 +414,7 @@ export default {
     start(index) {
       this.$confetti.start({
         canvasId: index,
-        particlesPerFrame: 5,
+        particlesPerFrame: 1,
       });
     },
   },
